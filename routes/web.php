46d9a254bb8fa44821e549919bfd8459e0a92ceb
MarkoSopta <77636140+MarkoSopta@users.noreply.github.com>
2024-03-30 16:10:34 +0100

    semi fukncionalni crud

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+
+    Route::resource('product', App\Http\Controllers\ProducttController::class);
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
